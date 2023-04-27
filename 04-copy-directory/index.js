8758fed874e5ecd84ab3cf00aab7e3da1f28a9fe
dima92 <dima-tkachenko@mail.ru>
2023-04-27 15:15:11 +0300

    feat: finish copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,50 @@
+const path = require("path");
+const fs = require("fs");
+const {promises: fsPromises} = require("fs");
 
+const destination = path.join(__dirname, "files-copy");
+const source = path.join(__dirname, "files");
+
+fs.access(destination, function (error) {
+  if (error) {
+    copyFiles();
+  } else {
+    deleteAndCopy();
+  }
+});
+
+async function deleteAndCopy() {
+  await fsPromises.rmdir(destination, { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  });
+  copyFiles();
+}
+
+function copyFiles() {
+  fs.mkdir(destination, { recursive: true }, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    fs.readdir(source, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.log(err);
+      } else {
+        files.forEach((file) => {
+          const fileName = file.name.toString();
+
+          fsPromises
+            .copyFile(
+              path.join(__dirname, "files", fileName),
+              path.join(__dirname, "files-copy", fileName)
+            )
+            .catch(function (error) {
+              console.log(error);
+            });
+        });
+        console.log("Folder Copied");
+      }
+    });
+  });
+}
